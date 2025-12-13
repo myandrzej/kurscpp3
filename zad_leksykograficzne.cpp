d9ad9d03abb845ae9021299223266a2e1917ae96
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-12-13 12:24:42 +0100

    Create zad_leksykograficzne.cpp

---
+++ zad_leksykograficzne.cpp
@@ -0,0 +1,9 @@
+Napisz program w C++, który wczyta od użytkownika dwa słowa i porówna je leksykograficznie. Program powinien wypisać:
+
+"Napisy sa rowne", jeśli oba słowa są identyczne,
+
+"Pierwsze slowo jest wieksze leksykograficznie", jeśli pierwsze słowo jest większe alfabetycznie,
+
+"Drugie slowo jest wieksze leksykograficznie", jeśli drugie słowo jest większe alfabetycznie.
+
+Użyj tablic znaków (char[]) oraz funkcji strcmp z biblioteki <cstring>.
