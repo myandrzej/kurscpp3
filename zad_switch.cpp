0c540a18050cf933cdd7f00618459b751a271970
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-12-13 12:53:36 +0100

    Create zad_switch.cpp

---
+++ zad_switch.cpp
@@ -0,0 +1,25 @@
+1. Napisz program z instrukcją switch wyświetlającą na podstawie zmiennej całkowitej nr nazwę miesiąca słownie.
+2. Napisz program kalkulator, który wykonuje wybraną przez użytkownika operacją arytmetyczną (suma,
+różnica, iloczyn, iloraz) na dwóch wczytanych liczbach. 
+3. Napisz program z instrukcją switch która na podstawie zmiennej całkowitej ocena wyświetla jedna z informacji: brak
+promocji do następnej klasy, promocja do następnej klasy, promocja z ocena celująca.
+4. Napisz program, który pobiera od użytkownika liczbę punktów zdobytych na kolokwium. Następnie
+sprawdza czy liczba punktów jest poprawna, jeśli nie to wyświetli odpowiedni komunikat, a jeśli tak,
+to korzystając z instrukcji switch, oblicza ocenę z kolokwium na podstawie zdobytych punktów
+zgodnie z następującą regułą:
+ 0-10 pkt – 2,
+ 11-13 pkt - 3,
+ 14-16 pkt - 4,
+ 17-18 pkt - 5
+
+5. Napisz program, który wczytuje kwotę w złotych, a następnie przelicza ją na taka walutę jaką wybierze
+użytkownik: funty, dolary, euro.
+
+6. Program dnia pewnego seminarium wygląda następująco:
+ 8-11 wykłady,
+ 12-13 dyskusje,
+ 14 obiad,
+ 15-18 prelekcje,
+ 19 kolacja.
+Program ma za zadanie pobrać godzinę i wyświetlić informacje o wszystkich punktach dnia, które
+jeszcze będą począwszy od danej godziny
